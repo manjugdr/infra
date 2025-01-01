af07598545e33a7fd1c4000ec801abbcf0ed627f
manjugdr <153181318+manjugdr@users.noreply.github.com>
2025-01-01 21:36:56 +0530

    Update main.tf

--- terraform_infra_aws/applications/stohrm1-0/environments/prod/ecr/main.tf
+++ terraform_infra_aws/applications/stohrm1-0/environments/prod/ecr/main.tf
@@ -1,5 +1,5 @@
 module "ecr" {
-  source = "../../../../modules/ecr"
+  source = "../../../../../modules/ecr"
   project_name = var.project_name  
   environment = var.environment
   repository_names = var.repository_names
