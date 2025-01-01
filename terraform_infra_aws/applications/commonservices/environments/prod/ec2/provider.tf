f6c64d3e62d229d9051c6730d7ea2918c66aa736
manjugdr <153181318+manjugdr@users.noreply.github.com>
2025-01-01 14:04:01 +0530

    Update provider.tf

--- terraform_infra_aws/applications/commonservices/environments/prod/ec2/provider.tf
+++ terraform_infra_aws/applications/commonservices/environments/prod/ec2/provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "ascent-terraform-statefile"
-    key    = "commonservices/prod/ec2/terraform.tfstate"
+    bucket = "commonservices1"
+    key    = "prod/ec2/terraform.tfstate"
     region = "ap-south-1"
   }
 }
