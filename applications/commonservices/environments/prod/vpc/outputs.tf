3b004cb51c948ee75de1fafbdcce5cfb00eb4111
manjugdr <153181318+manjugdr@users.noreply.github.com>
2025-01-01 12:24:59 +0530

    Add files via upload

---
+++ applications/commonservices/environments/prod/vpc/outputs.tf
@@ -0,0 +1,32 @@
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+output "public_subnet_ids" {
+  description = "The ID of the public subnet"
+  value       = module.vpc.public_subnet_ids
+}
+output "private_subnet_ids" {
+  description = "The ID of the private subnet"
+  value       = module.vpc.private_subnet_ids
+}
+output "private_subnet_tags" {
+  description = "The ID of the private subnet"
+  value       = module.vpc.private_subnet_tags
+}
+output "public_subnet_tags" {
+  description = "The ID of the private subnet"
+  value       = module.vpc.public_subnet_tags
+}
+output "public_subnet_cidr" {
+  description = "The ID of the private subnet"
+  value       = module.vpc.public_subnet_cidr
+}
+output "private_subnet_cidr" {
+  description = "The ID of the private subnet"
+  value       = module.vpc.private_subnet_cidr
+}
+output "private_subnet_availabilityzones" {
+  description = "The ID of the private subnet AZ"
+  value       = module.vpc.private_subnet_availabilityzones
+}
