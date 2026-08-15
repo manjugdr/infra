da3a3140b5e1d874029c5a69b7a327e0eee4d2b9
manjugdr <153181318+manjugdr@users.noreply.github.com>
2026-08-15 17:00:42 +0530

    Update main.tf

--- terraform_infra_aws/applications/commonservices/environments/dev/eks/main.tf
+++ terraform_infra_aws/applications/commonservices/environments/dev/eks/main.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "ascent-terraform-statefiles"
+    bucket = "ascent-terraform-statefiles2"
     key    = "commonservices/dev/vpc/terraform.tfstate"
     region = "ap-south-1"
   }
